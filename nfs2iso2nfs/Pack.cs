a651e7236dd0dce79b86771b714c35d70869380f
ZestyTS <ZestyTS@gmail.com>
2023-09-16 10:55:56 -0700

    Fixed class name, and made it so Logger is no longer required

--- nfs2iso2nfs/Pack.cs
+++ nfs2iso2nfs/Pack.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 namespace nfs2iso2nfs
 {
-    public class NfsIsoConverter
+    public class Pack
     {
         private Patch _patch = new("");
         private Nfs _nfs = new();
@@ -14,16 +14,16 @@ namespace nfs2iso2nfs
         private string _keyFile = ".." + Path.DirectorySeparatorChar + "code" + Path.DirectorySeparatorChar + "htk.bin";
         private string _isoFile = "game.iso";
         private string _wiiKeyFile = "wii_common_key.bin";
-        private readonly ILogger<NfsIsoConverter> _logger;
+        public ILogger<Pack>? Logger { get; set; }
 
-        public NfsIsoConverter(ILogger<NfsIsoConverter> logger)
+        public Pack(ILogger<Pack>? logger = null)
         {
-            _logger = logger;
+            Logger = logger;
         }
+
         public async Task Convert(string[] args)
         {
             _nfs = new Nfs();
-            _patch = new Patch(".." + Path.DirectorySeparatorChar + "code" + Path.DirectorySeparatorChar + "fw.img");
 
             if (!CheckArgs(args))
                 return;
@@ -44,9 +44,9 @@ namespace nfs2iso2nfs
 
             if (!_keepFiles)
             {
-                _logger.LogInformation("Deleting files!");
+                Logger?.LogInformation("Deleting files!");
                 await _nfs.DeleteFilesAsync();
-                _logger.LogInformation("Deleted!");
+                Logger?.LogInformation("Deleted!");
                  
             }
         }
@@ -54,52 +54,52 @@ namespace nfs2iso2nfs
         private async Task DecryptAsync()
         {
             var header = await ByteHelper.GetHeaderAsync(_nfs.Dir + Path.DirectorySeparatorChar + "hif_000000.nfs");
-            _logger.LogInformation("Combining NFS Files");
+            Logger?.LogInformation("Combining NFS Files");
             await _nfs.CombineNFSFilesAsync();
-            _logger.LogInformation("Combined");
+            Logger?.LogInformation("Combined");
              
 
-            _logger.LogInformation("Decrypting hif.nfs...");
+            Logger?.LogInformation("Decrypting hif.nfs...");
             NfsHelper.DecryptNFS(_nfs.Hif, _nfs.HifDec, _nfs.Key, _nfs.SectorSize);
-            _logger.LogInformation("Decrypted!");
+            Logger?.LogInformation("Decrypted!");
              
 
-            _logger.LogInformation("Unpacking nfs");
+            Logger?.LogInformation("Unpacking nfs");
             _nfs.Unpack(header);
-            _logger.LogInformation("Unpacked");
+            Logger?.LogInformation("Unpacked");
              
 
-            _logger.LogInformation("Manipulate Iso - Decrypt");
+            Logger?.LogInformation("Manipulate Iso - Decrypt");
             NfsHelper.DecryptManipulateIso(_nfs.HifUnpack, _isoFile, _nfs.SectorSize, _nfs.CommonKey);
-            _logger.LogInformation("Felt up Iso");
+            Logger?.LogInformation("Felt up Iso");
              
         }
 
         private async Task EncryptAsync()
         {
-            _logger.LogInformation("Do Patching if applicable!");
+            Logger?.LogInformation("Do Patching if applicable!");
             await _patch.DoThePatchingAsync();
-            _logger.LogInformation("Patching Done!");
+            Logger?.LogInformation("Patching Done!");
              
 
-            _logger.LogInformation("Manipulate Iso - Encrypt");
+            Logger?.LogInformation("Manipulate Iso - Encrypt");
             var size = NfsHelper.EncryptManipulateIso(_isoFile, _nfs.HifUnpack, _nfs.SectorSize, _nfs.CommonKey);
-            _logger.LogInformation("Felt up Iso");
+            Logger?.LogInformation("Felt up Iso");
              
 
-            _logger.LogInformation("Packing nfs");
+            Logger?.LogInformation("Packing nfs");
             var header = _nfs.PackNFS(size);
-            _logger.LogInformation("Packing complete!");
+            Logger?.LogInformation("Packing complete!");
              
 
-            _logger.LogInformation("EncryptNFS");
+            Logger?.LogInformation("EncryptNFS");
             NfsHelper.EncryptNFS(_nfs.HifDec, _nfs.Hif, _nfs.Key, _nfs.SectorSize, header);
-            _logger.LogInformation("Encrypted!");
+            Logger?.LogInformation("Encrypted!");
              
 
-            _logger.LogInformation("Split NFS File");
-
-            _logger.LogInformation("Splitted!");
+            Logger?.LogInformation("Split NFS File");
+            await _nfs.SplitFileAsync();
+            Logger?.LogInformation("Splitted!");
              
         }
 
@@ -177,26 +177,26 @@ namespace nfs2iso2nfs
                         break;
 
                     case "-help":
-                        _logger.LogInformation("+++++ NFS2ISO2NFS v0.6 +++++");
+                        Logger?.LogInformation("+++++ NFS2ISO2NFS v0.6 +++++");
                          
-                        _logger.LogInformation("-dec            Decrypt .nfs files to an .iso file.");
-                        _logger.LogInformation("-enc            Encrypt an .iso file to .nfs file(s)");
-                        _logger.LogInformation("-key <file>     Location of AES key file. DEFAULT: code" + Path.DirectorySeparatorChar + "htk.bin.");
-                        _logger.LogInformation("-wiikey <file>  Location of Wii Common key file. DEFAULT: wii_common_key.bin.");
-                        _logger.LogInformation("-iso <file>     Location of .iso file. DEFAULT: game.iso.");
-                        _logger.LogInformation("-nfs <file>     Location of .nfs files. DEFAULT: current Directory.");
-                        _logger.LogInformation("-fwimg <file>   Location of fw.img. DEFAULT: code" + Path.DirectorySeparatorChar + "fw.img.");
-                        _logger.LogInformation("-keep           Don't delete the files produced in intermediate steps.");
-                        _logger.LogInformation("-legit          Don't patch fw.img to allow fakesigned content");
-                        _logger.LogInformation("-lrpatch        Map emulated Classic Controller's L & R to Gamepad's ZL & ZR");
-                        _logger.LogInformation("-wiimote        Emulate a Wii Remote instead of the Classic Controller");
-                        _logger.LogInformation("-horizontal     Remap Wii Remote d-pad for horizontal usage (implies -wiimote)");
-                        _logger.LogInformation("-homebrew       Various patches to enable proper homebrew functionality");
-                        _logger.LogInformation("-passthrough    Allow homebrew to keep using normal wiimotes with gamepad enabled");
-                        _logger.LogInformation("-instantcc      Report emulated Classic Controller at the very first check");
-                        _logger.LogInformation("-nocc           Report that no Classic Controller is connected");
-                        _logger.LogInformation("-output         Location of where the NFS files will be outputted to. DEFAULT: code" + Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar);
-                        _logger.LogInformation("-help           Print this text.");
+                        Logger?.LogInformation("-dec            Decrypt .nfs files to an .iso file.");
+                        Logger?.LogInformation("-enc            Encrypt an .iso file to .nfs file(s)");
+                        Logger?.LogInformation("-key <file>     Location of AES key file. DEFAULT: code" + Path.DirectorySeparatorChar + "htk.bin.");
+                        Logger?.LogInformation("-wiikey <file>  Location of Wii Common key file. DEFAULT: wii_common_key.bin.");
+                        Logger?.LogInformation("-iso <file>     Location of .iso file. DEFAULT: game.iso.");
+                        Logger?.LogInformation("-nfs <file>     Location of .nfs files. DEFAULT: current Directory.");
+                        Logger?.LogInformation("-fwimg <file>   Location of fw.img. DEFAULT: code" + Path.DirectorySeparatorChar + "fw.img.");
+                        Logger?.LogInformation("-keep           Don't delete the files produced in intermediate steps.");
+                        Logger?.LogInformation("-legit          Don't patch fw.img to allow fakesigned content");
+                        Logger?.LogInformation("-lrpatch        Map emulated Classic Controller's L & R to Gamepad's ZL & ZR");
+                        Logger?.LogInformation("-wiimote        Emulate a Wii Remote instead of the Classic Controller");
+                        Logger?.LogInformation("-horizontal     Remap Wii Remote d-pad for horizontal usage (implies -wiimote)");
+                        Logger?.LogInformation("-homebrew       Various patches to enable proper homebrew functionality");
+                        Logger?.LogInformation("-passthrough    Allow homebrew to keep using normal wiimotes with gamepad enabled");
+                        Logger?.LogInformation("-instantcc      Report emulated Classic Controller at the very first check");
+                        Logger?.LogInformation("-nocc           Report that no Classic Controller is connected");
+                        Logger?.LogInformation("-output         Location of where the NFS files will be outputted to. DEFAULT: code" + Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar);
+                        Logger?.LogInformation("-help           Print this text.");
                         return false;
                     default:
                         break;
@@ -224,51 +224,51 @@ namespace nfs2iso2nfs
         {
             if (_patch.MapShoulderToTrigger && (_patch.HorizWiimote || _patch.VertWiimote))
             {
-                _logger.LogInformation("ERROR: Please don't mix patches for Classic Controller and Wii Remote.");
+                Logger?.LogInformation("ERROR: Please don't mix patches for Classic Controller and Wii Remote.");
                 return false;
             }
 
             if (!_enc && File.Exists(_nfs.Dir + Path.DirectorySeparatorChar + "hif_000000.nfs"))
             {
-                _logger.LogInformation("+++++ NFS2ISO +++++");
+                Logger?.LogInformation("+++++ NFS2ISO +++++");
 
                 if (!_enc && !File.Exists(_nfs.Dir + Path.DirectorySeparatorChar + "hif_000000.nfs"))
                 {
-                    _logger.LogInformation("ERROR: .nfs files not found! Exiting...");
+                    Logger?.LogInformation("ERROR: .nfs files not found! Exiting...");
                     return false;
                 }
                 else if (!_enc && File.Exists(_nfs.Dir + Path.DirectorySeparatorChar + "hif_000000.nfs"))
                 {
-                    _logger.LogInformation("You haven't specified if you want to use nfs2iso or iso2nfs");
-                    _logger.LogInformation("Found .nfs files! Assuming you want to use nfs2iso...");
+                    Logger?.LogInformation("You haven't specified if you want to use nfs2iso or iso2nfs");
+                    Logger?.LogInformation("Found .nfs files! Assuming you want to use nfs2iso...");
                     _enc = false;
                 }
             }
             else if (_enc && File.Exists(_isoFile))
             {
-                _logger.LogInformation("+++++ ISO2NFS +++++");
+                Logger?.LogInformation("+++++ ISO2NFS +++++");
 
                 if (_enc && !File.Exists(_isoFile))
                 {
-                    _logger.LogInformation("ERROR: .iso file not found! Exiting...");
+                    Logger?.LogInformation("ERROR: .iso file not found! Exiting...");
                     return false;
                 }
                 if (_enc && !File.Exists(_patch.FwFile))
                 {
-                    _logger.LogInformation("ERROR: fw.img not found! Exiting...");
+                    Logger?.LogInformation("ERROR: fw.img not found! Exiting...");
                     return false;
                 }
                 else if (_enc && File.Exists(_isoFile))
                 {
-                    _logger.LogInformation("You haven't specified if you want to use nfs2iso or iso2nfs");
-                    _logger.LogInformation("Found .iso file!  Assuming you want to use iso2nfs...");
+                    Logger?.LogInformation("You haven't specified if you want to use nfs2iso or iso2nfs");
+                    Logger?.LogInformation("Found .iso file!  Assuming you want to use iso2nfs...");
                     _enc = true;
                 }
             }
             else
             {
-                _logger.LogInformation("You haven't specified if you want to use nfs2iso or iso2nfs");
-                _logger.LogInformation("Found neither .iso nor .nfs files! Check -help for usage of this program.");
+                Logger?.LogInformation("You haven't specified if you want to use nfs2iso or iso2nfs");
+                Logger?.LogInformation("Found neither .iso nor .nfs files! Check -help for usage of this program.");
                 return false;
             }
             return true;
@@ -276,47 +276,47 @@ namespace nfs2iso2nfs
 
         private async Task<byte[]?> GetKeyAsync()
         {
-            _logger.LogInformation("Searching for AES key file...");
+            Logger?.LogInformation("Searching for AES key file...");
             if (!File.Exists(_keyFile))
             {
-                _logger.LogInformation("ERROR: Could not find AES key file! Exiting...");
+                Logger?.LogInformation("ERROR: Could not find AES key file! Exiting...");
                 return null;
             }
             var key = await KeyHelper.GetKeyAsync(_keyFile);
             if (key == null)
             {
-                _logger.LogInformation("ERROR: AES key file has wrong file size! Exiting...");
+                Logger?.LogInformation("ERROR: AES key file has wrong file size! Exiting...");
                 return null;
             }
-            _logger.LogInformation("AES key file found!");
+            Logger?.LogInformation("AES key file found!");
 
             if (_nfs.CommonKey[0] != 0xeb)
             {
-                _logger.LogInformation("Wii common key not found in source code. Looking for file...");
+                Logger?.LogInformation("Wii common key not found in source code. Looking for file...");
                 if (!File.Exists(_wiiKeyFile))
                 {
                     _nfs.CommonKey = ConvertHexStringToByteArray("ebe42a225e8593e448d9c5457381aaf7");
                     if (_nfs.CommonKey[0] != 0xeb)
                     {
-                        _logger.LogInformation("ERROR: Could not find Wii common key file! Exiting...");
+                        Logger?.LogInformation("ERROR: Could not find Wii common key file! Exiting...");
                         return null;
                     }
                     else
-                        _logger.LogInformation("Wii common key has been found.");
+                        Logger?.LogInformation("Wii common key has been found.");
                 }
                 else
                 {
                     _nfs.CommonKey = await KeyHelper.GetKeyAsync(_wiiKeyFile) ?? Array.Empty<byte>();
                     if (_nfs.CommonKey == null)
                     {
-                        _logger.LogInformation("ERROR: Wii common key file has wrong file size! Exiting...");
+                        Logger?.LogInformation("ERROR: Wii common key file has wrong file size! Exiting...");
                         return null;
                     }
-                    _logger.LogInformation("Wii Common Key file found!");
+                    Logger?.LogInformation("Wii Common Key file found!");
                 }
             }
             else
-                _logger.LogInformation("Wii common key found in source code!");
+                Logger?.LogInformation("Wii common key found in source code!");
 
             return key;
         }
